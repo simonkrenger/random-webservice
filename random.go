9c6c80a48c6ac4eb072062c7034a08b44fe61784
Simon Krenger <simon@krenger.ch>
2021-02-09 22:16:57 +0100

    Add help message to / endpoint

--- random.go
+++ random.go
@@ -1,16 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "math/rand"
-import "strconv"
+import (
+  "github.com/gin-gonic/gin"
+  "math/rand"
+  "strconv"
+)
 
 func main() {
   r := gin.Default()
+
+  r.GET("/", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "Hello. Try /random",
+    })
+  })
+
   r.GET("/random", func(c *gin.Context) {
   	n := rand.Intn(100)
     c.JSON(200, gin.H{
       "number": strconv.Itoa(n),
     })
   })
+
   r.Run() // listen and serve on 0.0.0.0:8080
 }
